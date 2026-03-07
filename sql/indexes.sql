039b8876dfd1452fb66a78598bc5e24ddf2797f7
itan-iniad <s1f102201605@iniad.org>
2026-03-08 04:09:31 +0900

    perf: add MySQL indexes via indexes.sql

---
+++ sql/indexes.sql
@@ -0,0 +1,10 @@
+USE isuconp;
+
+ALTER TABLE comments
+  ADD INDEX idx_comments_post_created (post_id, created_at DESC);
+
+ALTER TABLE posts
+  ADD INDEX idx_posts_created (created_at DESC);
+
+ALTER TABLE posts
+  ADD INDEX idx_posts_user (user_id);
