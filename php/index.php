c56ee7125ae475bc1bd47312813e754cfb3e118d
itan-iniad <s1f102201605@iniad.org>
2026-04-07 15:11:03 +0900

    tighten php passhash comparison

--- php/index.php
+++ php/index.php
@@ -106,7 +106,7 @@ $container->set('helper', function ($c) {
 
         public function try_login($account_name, $password) {
             $user = $this->fetch_first('SELECT * FROM users WHERE account_name = ? AND del_flg = 0', $account_name);
-            if ($user !== false && calculate_passhash($user['account_name'], $password) == $user['passhash']) {
+            if ($user !== false && calculate_passhash($user['account_name'], $password) === $user['passhash']) {
                 return $user;
             }
             return null;
