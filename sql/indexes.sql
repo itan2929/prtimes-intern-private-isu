c28c66d3eeee18f98107ac230d0a28684d4b9426
itan-iniad <s1f102201605@iniad.org>
2026-04-09 01:31:35 +0900

    add posts created_at index

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -4,8 +4,8 @@ ALTER TABLE comments
   ADD INDEX idx_comments_post_created (post_id, created_at DESC),
   ADD INDEX idx_comments_user (user_id);
 
--- ALTER TABLE posts
---   ADD INDEX idx_posts_created (created_at DESC);
+ALTER TABLE posts
+  ADD INDEX idx_posts_created (created_at DESC);
 
 -- ALTER TABLE posts
 --   ADD INDEX idx_posts_user (user_id);
